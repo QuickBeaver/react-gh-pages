5fada2c1c0b81c40c84f1f4d61911f4ca354fc78
LEGOUEST <clement.legouest@cgi.com>
2022-05-25 12:14:47 +0200

    Oracle basic page

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,28 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
-import Manatee from './components/Manatee/Manatee';
+import Oracle from './components/oracle/Oracle';
 import Narwhal from './components/Narwhal/Narwhal';
 import Whale from './components/Whale/Whale';
 
 function App() {
   return (
-    <div className="App">
+    <div className="wrapper">
       <header className="App-header">
         <h1 className='App-title'>QuickBeaver</h1>
         <nav className="header-menu">
-          <li><a href="/manatee">manatee</a></li>
+          <li><a href="/oracle">oracle</a></li>
           <li><a href="/narwhal">narwhal</a></li>
           <li><a href="/whale">whale</a></li>
         </nav>
       </header>
-      <div className="wrapper">
-        <h1>Marine mammals</h1>
-      </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/oracle' element={<Oracle />}></Route>
+        <Route path='/narwhal' element={<Narwhal />}></Route>
+        <Route path='/whale' element={<Whale />}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
